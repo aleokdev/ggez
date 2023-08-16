de4cf1a9b34412fab2131911f00909411b2bb3e8
aleokdev <aleok.inf@gmail.com>
2023-08-16 17:59:58 +0200

    Update pitch dynamically on set_pitch

--- src/audio.rs
+++ src/audio.rs
@@ -369,7 +369,8 @@ impl SoundSource for Source {
         self.state.set_start(dur)
     }
     fn set_pitch(&mut self, ratio: f32) {
-        self.state.set_pitch(ratio)
+        self.state.set_pitch(ratio);
+        self.sink.set_speed(ratio);
     }
     fn repeat(&self) -> bool {
         self.state.repeat()
